1130b39536eddfe083399b7d833d433036fcc738
Naoto Matsumoto <s1F102001243@iniad.org>
2022-10-19 15:38:17 +0900

    CREATED slamy app with django-admin command

---
+++ slamy/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Word(models.Model):
+    word = models.CharField(max_length=50, null=False, blank=False)
+    short_description = models.CharField(max_length=100, null=False, blank=False)
+    detailed_description = models.TextField(max_length=500, null=True, blank=False)
+    image_url = models.ImageField(upload_to='images/', null=True, blank=True)
+    source = models.ForeignKey('Source', on_delete=models.CASCADE, to_field='pk', null=False, blank=False)
+
+
+class Source(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    url = models.URLField(null=True, blank=True)
+    author = models.CharField(max_length=50, null=False, blank=False)
