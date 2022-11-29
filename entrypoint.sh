9df80042334c7458739a63824a6b562f55dcb238
Naoto Matsumoto <s1F102001243@iniad.org>
2022-11-30 06:09:38 +0900

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,9 @@
 
 python manage.py migrate --noinput
 python manage.py collectstatic --no-input --clear
+
+if "$DEBUG"; then
+    exec python3 manage.py runserver 0.0.0.0:8000
+else
+    exec "$@"
+fi
