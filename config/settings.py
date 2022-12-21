e915081ef6f2d1f88af2b152119903827524dd5c
Naoto Matsumoto <s1F102001243@iniad.org>
2022-12-21 14:05:55 +0900

    CSRF_TRUSTED_ORIGINとCORS_ORIGIN_WHITELISTにまつわるエラーを修正しました

--- config/settings.py
+++ config/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(',')
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1']
 
 
 # django-cors-headers
 # Allow all hosts
-CORS_ORIGIN_WHITELIST = ('*')
+CORS_ORIGIN_WHITELIST = ['http://localhost']
